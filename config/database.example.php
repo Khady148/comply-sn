4668956de141517106cc875b24455723589f8a63
Khady Diop Diombokho <diombokhokhadija148@gmail.com>
2026-08-14 12:29:46 +0000

    Ajout du modèle de configuration MySQL

---
+++ config/database.example.php
@@ -0,0 +1,27 @@
+<?php
+
+$host = "localhost";
+$dbname = "comply_sn";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+} catch (PDOException $e) {
+
+    die(
+        "Erreur de connexion à la base de données : "
+        . $e->getMessage()
+    );
+
+}
